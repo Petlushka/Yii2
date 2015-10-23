a90c9f1702086bc9ac9f5f512a440c880d7e3445
Petlushka <petlushka@rambler.ru>
2015-10-23 19:18:15 +0300

    Change web.php

--- config/web.php
+++ config/web.php
@@ -8,7 +8,6 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         'urlManager' => [
-
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
