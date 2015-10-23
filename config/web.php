135d200d414a558eacc4c4458c485c922ee66811
Petlushka <petlushka@rambler.ru>
2015-10-23 19:02:17 +0300

    second

--- config/web.php
+++ config/web.php
@@ -7,6 +7,9 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'urlManager' => [
+
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'vDvz_83i1Ed_Dto-nhtSiA393ZvoaOE9',
